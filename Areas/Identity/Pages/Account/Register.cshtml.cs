f1e3c46a0600945df630be53117b6627f1486bf9
debiroz10 <debiroz10@gmail.com>
2023-12-03 15:12:13 +0200

    LOGIN

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -70,23 +70,23 @@ namespace CloudSaba.Areas.Identity.Pages.Account
         /// </summary>
         public class InputModel
         {
-            ///// <summary>
-            /////     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            /////     directly from your code. This API may change or be removed in future releases.
-            ///// </summary>
-            [Required]
-            [StringLength(255, ErrorMessage = "The FirstName Field  should have a maximum of 255 characters")]
-            [Display(Name = "FirstName")]
-            public string FirstName { get; set; }
+            /////// <summary>
+            ///////     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+            ///////     directly from your code. This API may change or be removed in future releases.
+            /////// </summary>
+            //[Required]
+            //[StringLength(255, ErrorMessage = "The FirstName Field  should have a maximum of 255 characters")]
+            //[Display(Name = "FirstName")]
+            //public string FirstName { get; set; }
 
-            ///// <summary>
-            /////     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            /////     directly from your code. This API may change or be removed in future releases.
-            ///// </summary>
-            [Required]
-            [StringLength(255, ErrorMessage = "The FirstName Field  should have a maximum of 255 characters")]
-            [Display(Name = "LastName")]
-            public string LastName { get; set; }
+            /////// <summary>
+            ///////     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+            ///////     directly from your code. This API may change or be removed in future releases.
+            /////// </summary>
+            //[Required]
+            //[StringLength(255, ErrorMessage = "The FirstName Field  should have a maximum of 255 characters")]
+            //[Display(Name = "LastName")]
+            //public string LastName { get; set; }
 
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
